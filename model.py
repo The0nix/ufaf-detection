85f65905bddb00edff4813446abc1aee380fd4e7
topshik <semenkin.anton@gmail.com>
2020-05-30 14:42:43 +0300

    fix: filename add: detector / feature extractor separation

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 import torch
 from torch import nn
-from torchvision.models import vgg16
 
 
 class EarlyFusion(nn.Module):
     def __init__(self, img_depth, n_base_channels=32, n_time_steps=5):
         super().__init__()
+        self.n_base_channels = n_base_channels
+        self.out_channels = n_base_channels * 8
         self.conv1d = nn.Conv1d(n_time_steps, 1, kernel_size=1, bias=False)
         self.vgg_modules = nn.Sequential(
             nn.Conv2d(img_depth, n_base_channels, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
@@ -31,7 +32,8 @@ class EarlyFusion(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(n_base_channels * 8, n_base_channels * 8, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),)
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+        )
 
     def forward(self, frames):
         batch_size, n_time_steps, pic_size = frames.shape[0], frames.shape[1], frames.shape[2:]
@@ -41,3 +43,25 @@ class EarlyFusion(nn.Module):
         vgg_out = self.vgg_modules(fused)
         return vgg_out
 
+
+class Detector(nn.Module):
+    def __init__(self, img_depth, n_predefined_boxes=6):
+        """
+        :param img_depth: discretized height of the image from BEV
+        :param n_predefined_boxes: number of bounding boxed corresponding to each cell of the feature map
+
+        First `6 * n_predefined_boxes` depth levels of final_conv output correspond to BB location parameters
+        on the original img, last `n_predefined_boxes` depth levels correspond to classification probabilities of
+        BB containing a vehicle
+        """
+        super().__init__()
+        self.feature_extractor = EarlyFusion(img_depth)
+
+        self.final_conv = nn.Conv2d(self.feature_extractor.out_chanels, 7 * n_predefined_boxes,
+                                    kernel_size=3, padding=1)
+
+    def forward(self, frames):
+        feature_map = self.feature_extractor(frames)
+        output = self.final_conv(feature_map)
+
+        return output
