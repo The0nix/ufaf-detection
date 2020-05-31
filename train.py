31ed86dac41f3fe008f916c889657e29032abc8b
The0nix <nektonikto999@gmail.com>
2020-05-31 17:54:45 +0300

    Add TODO to remove kwargs

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
-def train(data_path:str, **kwargs):
+def train(data_path:str, **kwargs):  # TODO: remove **kwargs and add proper keyword arguments
     raise NotImplementedError
 
 
-def eval(model_path: str, data_path:str, **kwargs):
+def eval(model_path: str, data_path:str, **kwargs):  # TODO: remove **kwargs and add proper keyword arguments
     raise NotImplementedError
