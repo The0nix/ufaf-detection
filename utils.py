af295256a4c29044140782d5d13ff8d52a53e8bd
The0nix <nektonikto999@gmail.com>
2020-06-02 23:02:40 +0300

    change IoU calculation to simpler one without Shapely

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 import numpy as np
 
 
@@ -8,7 +10,7 @@ def bbox_to_coordinates(bbox: np.ndarray, rot: bool = True):
     :param bbox: array of ground truth bounding box geometrical parameters (center_y, center_x,
     width, length, sin(a), cos(a))
     :param rot: perform rotation
-    :return: list of 4 tuples of (x, y) coordinates: [left_top, right_top, right_bottom, left_bottom]
+    :return: list of 4 tuples of (y, x) coordinates: [left_top, right_top, right_bottom, left_bottom]
     """
     y, x = np.asarray(bbox[:2])
     width, length = bbox[2:4]  # converts borders calculus to center
@@ -29,3 +31,23 @@ def bbox_to_coordinates(bbox: np.ndarray, rot: bool = True):
 
     vertices = np.asarray(vertices)
     return vertices
+
+
+def calc_iou(box1: List[Tuple[int, int]], box2: List[Tuple[int, int]]) -> float:
+    """
+    Calculate IoU of two boxes presented by their corners' coordinates
+    :param box1: list of 4 tuples of (x, y) coordinates: [left_top, right_top, right_bottom, left_bottom]
+    :param box2: list of 4 tuples of (x, y) coordinates: [left_top, right_top, right_bottom, left_bottom]
+    :return: intersection over union score
+    """
+
+    x1 = max(box1[0][1], box2[0][1])  # left top angle of the intersection
+    y1 = max(box1[0][0], box2[0][0])  #
+    x2 = min(box1[2][1], box2[2][1])  # right bottom angle of the intersection
+    y2 = min(box1[2][0], box2[2][0])  #
+
+    box1_area = (box1[0][1] - box1[2][1]) * (box1[0][0] - box1[2][0])
+    box2_area = (box2[0][1] - box2[2][1]) * (box2[0][0] - box2[2][0])
+    intersection = (x2 - x1) * (y2 - y1) if (y2 > y1 and x2 > x1) else 0
+    union = box1_area + box2_area - intersection
+    return intersection / union
